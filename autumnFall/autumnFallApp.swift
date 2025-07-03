9f959ffaeff6ba751fac76b4b0d35811bc0bb719
KhairulHafis <khairulhafis07@outlook.sg>
2025-07-03 22:43:07 +0800

    Initial Commit

---
+++ autumnFall/autumnFallApp.swift
@@ -0,0 +1,17 @@
+//
+//  autumnFallApp.swift
+//  autumnFall
+//
+//  Created by Muhammad Khairul Hafis Bin Hussain on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct autumnFallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
